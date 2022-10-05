9d8319303a9da5da9203123feacdad5462f70768
AURYSS <114744861+AURYSS@users.noreply.github.com>
2022-10-05 08:28:48 -0500

    Create hoy.php

---
+++ parcial_2/hoy.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <body bgcolor="#00FF00" o bgcolor="Lime" a la etiqueta </body>.
+</head>
+<body>
+    <?php
+    echo '<center><FONT SIZE=3>!!28 DE SEPTIEMBRE DE 2022¡¡ 8:20 am </font><FONT SIZE=4></center>';
+    echo '<center>';
+    echo '<h1>';
+    echo '!!28 DE SEPTIEMBRE DE 2022¡¡ 8:20 am';
+    echo '</h1>';
+    echo '</center>';
+    echo '<center></font><FONT SIZE=5>!!28 DE SEPTIEMBRE DE 2022¡¡ 8:20 am </font></center>';
+    ?>
+</body>
+</html>
